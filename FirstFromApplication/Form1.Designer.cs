b3f0cf0329617b14cc95b4a039ea2775454ee64e
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-19 09:38:05 +0900

    202020519

--- FirstFromApplication/Form1.Designer.cs
+++ FirstFromApplication/Form1.Designer.cs
@@ -20,21 +20,7 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 디자이너에서 생성한 코드
-
-        /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
-
-        #endregion
+        private System.Windows.Forms.Button button1;
     }
 }
 
